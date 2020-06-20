33fdd2dfc473f520f22830e09db99dc68b3ac2f6
Mridul++ <36070673+Mridulupad@users.noreply.github.com>
2020-06-20 12:12:36 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -29,4 +29,13 @@ self.addEventListener("fetch", fetchEvent => {
       })
     )
   })
-  self.addEventListener('fetch', function(event) {});
+//   self.addEventListener('fetch', function(event) {});
+if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(reg){
+        console.log("Yes, it did.");
+     }).catch(function(err) {
+        console.log("No it didn't. This happened:", err)
+    });
+ }
