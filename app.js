52a4977805e4a411bc81655e142110ed86d0b020
Mridul++ <36070673+Mridulupad@users.noreply.github.com>
2020-06-22 23:30:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,64 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" },
-]
-
+  {
+    name: "REACT",
+    image: "/React_Native_Logo.png"
+  },
+  {
+    name: "ANGULAR",
+    image: "/461803-angular-JavaScript-HTML.jpg"
+  },
+  {
+    name: "JAVASCRIPT",
+    image: "/JavaScript-Wallpapers-Wallpaper-Cave.jpg"
+  },
+  {
+    name: "Vue.js",
+    image: "/maxresdefault.jpg"
+  },
+  {
+    name: "SPRING BOOT",
+    image: "/0_Rmf52gWzf3hoj4eW.jpg"
+  },
+  {
+    name: "JAVA",
+    image: "/383133.jpg"
+  },
+  {
+    name: "Inventore",
+    image: "/coffee7.jpg"
+  },
+  {
+    name: "Veritatis",
+    image: "/coffee8.jpg"
+  },
+  {
+    name: "Accusantium",
+    image: "/coffee9.jpg"
+  }
+];
 const showCoffees = () => {
-    let output = ""
-    coffees.forEach(
-      ({ name, image }) =>
-        (output += `
-                <div class="card">
-                  <img class="card--avatar" src=${image} />
-                  <h1 class="card--title">${name}</h1>
-                  <a class="card--link" href="#">Taste</a>
-                </div>
-                `)
-    )
-    container.innerHTML = output
-  }
-  
-  document.addEventListener("DOMContentLoaded", showCoffees)
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  );
+  container.innerHTML = output;
+};
 
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
-  }
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
